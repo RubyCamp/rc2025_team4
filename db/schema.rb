b0e2caed4f03ba3a26c0a3862ff4c08daa39c480
rikuro00 <rikuro001@gmail.com>
2025-08-27 01:08:14 +0000

    温泉→グルメ

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[8.0].define(version: 2025_08_27_110000) do
-
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_075015) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -53,9 +51,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_110000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
-    t.string "sales_s"
-    t.string "sales_f"
-    t.string "holiday"
     t.string "address"
   end
 
