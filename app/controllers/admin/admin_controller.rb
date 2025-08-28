a931b03718ff5773f44165b70500907483870294
sasasa <r6_ooguni@icc.core.ac.jp>
2025-08-28 15:42:45 +0000

    ログイン画面完成

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,2 +1,15 @@
+# app/controllers/admin/admin_controller.rb
 class Admin::AdminController < ApplicationController
+  before_action :require_admin_password
+
+
+
+
+
+  private
+
+  def require_admin_password
+    return if session[:admin_authenticated]
+    redirect_to admin_login_path
+  end
 end
