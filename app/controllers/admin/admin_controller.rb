74575a08a6670d15ca99cba8a0adc9e62e47311e
sasasa <r6_ooguni@icc.core.ac.jp>
2025-08-28 14:15:44 +0000

    一応戻しました。動きます

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,20 +1,2 @@
 class Admin::AdminController < ApplicationController
-  before_action :require_admin_login
-
-  private
-
-  def require_admin_login
-    # パスワードが params にある場合はチェック
-    if params[:password].present?
-      if params[:password] == ENV["ADMIN_PASSWORD"]
-        # 成功なら処理続行
-        return
-      else
-        flash.now[:alert] = "パスワードが違います"
-      end
-    end
-
-    # パスワードが未入力または間違いの場合はログイン画面を表示
-    render "admin/sessions/new"
-  end
 end
--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,2 @@
 class Admin::BaseController < ApplicationController
-  before_action :require_login
-
-  private
-
-  def require_login
-    unless session[:admin_logged_in]
-      redirect_to admin_login_path, alert: "ログインしてください"
-    end
-  end
 end
