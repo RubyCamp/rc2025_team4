63bc5417a050450d6072147de4a1d739c141a1d5
ny-si <zhikichang0@gmail.com>
2025-08-28 06:15:21 +0000

    fix: グルメ管理から管理画面に変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,7 +20,7 @@ module ApplicationHelper
   # @return [Array<Hash>] 管理サイト用ナビゲーションリンク
   def admin_navigation_links(current_path)
     create_navigation_link_set([
-      { path: "/admin/onsens", label: t("views.navigation.onsen_management"), aria_label: "グルメ管理", current_check: -> { current_path.start_with?("/admin/onsens") } },
+      { path: "/admin/onsens", label: t("views.navigation.onsen_management"), aria_label: "管理画面", current_check: -> { current_path.start_with?("/admin/onsens") } },
       { path: "/", label: t("views.navigation.public_site"), aria_label: "公開サイト", current_check: -> { false } }
     ])
   end
